da4303afbd18dc285b53e74960034231a5200b3f
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-06-16 11:07:15 -0300

    Adding light sensor

--- rocketsense/rocketsense.ino
+++ rocketsense/rocketsense.ino
@@ -1,16 +1,22 @@
 #define LM35 (A0)
+#define LDR (A1)
 
 float temperature;
+float light;
 
 void setup()
 {
     pinMode(LM35, INPUT);
+    pinMode(LDR, INPUT);
     Serial.begin(9600);
 }
 
 void loop()
 {
     temperature = (4 * analogRead(LM35) * 100.0) / 1024;
+    light = map(analogRead(LDR), 0, 1024, 0, 5000) / 1000.0;
     Serial.print(temperature);
-    Serial.println(" oC");
+    Serial.print(" oC\t");
+    Serial.print(light);
+    Serial.println(" lms");
 }
