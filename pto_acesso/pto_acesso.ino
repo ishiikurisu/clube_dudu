2b3962eaee49442aea44a5515ebb5c85e0660976
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-10-18 11:18:14 -0200

    Reading numbers from dialer

--- pto_acesso/pto_acesso.ino
+++ pto_acesso/pto_acesso.ino
@@ -1,23 +1,76 @@
 #define PULSE 3
 #define ACTIVITY 2
 #define LED 13
+#define STANDBY 1
+#define DIALING 2
+#define DIALED 4
+
+/* #######
+   # I/O #
+   ####### */
+
+bool generated_input = false;
+int dialed_number = -1;
+
+void readInput()
+{
+    static int state = STANDBY;
+    bool pulse = digitalRead(PULSE);
+    bool activity = digitalRead(ACTIVITY);
+
+    switch (state)
+    {
+        case STANDBY:
+            if (pulse == HIGH) {
+                state = DIALING;
+                dialed_number = 0;
+            }
+        break;
+
+        case DIALING:
+            if (pulse == LOW) {
+                state = STANDBY;
+                generated_input = true;
+            }
+            else if (activity == LOW) {
+                state = DIALED;
+                dialed_number++;
+            }
+        break;
+
+        case DIALED:
+            if (activity == HIGH) {
+                state = DIALING;
+            }
+        break;
+    }
+}
+
+void writeOutput()
+{
+    if ((generated_input) && (dialed_number > 0)) {
+        dialed_number = (dialed_number == 10)? 0 : dialed_number;
+        Serial.print("You dialed ");
+        Serial.println(dialed_number);
+        generated_input = false;
+        dialed_number = -1;
+    }
+}
+
+/* ###################
+   # MAIN PROCEDURES #
+   ################### */
 
 void setup()
 {
     pinMode(PULSE, INPUT);
     pinMode(ACTIVITY, INPUT);
     pinMode(LED, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    bool pulse = digitalRead(PULSE);
-    bool activity = digitalRead(ACTIVITY);
-    bool output = LOW;
-
-    if (pulse && !activity) {
-        output = HIGH;
-    }
-
-    digitalWrite(LED, output);
+    readInput();
+    writeOutput();
 }
