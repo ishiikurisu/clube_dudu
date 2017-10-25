f3cba82fbfa39cea9e5a7749e0fc1ab8f62d53bf
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-10-25 10:27:02 -0200

    Adding password logic

--- pto_acesso/pto_acesso.ino
+++ pto_acesso/pto_acesso.ino
@@ -4,6 +4,7 @@
 #define STANDBY 1
 #define DIALING 2
 #define DIALED 4
+#define SIZE 6
 
 /* #######
    # I/O #
@@ -11,6 +12,7 @@
 
 bool generated_input = false;
 int dialed_number = -1;
+int* password;
 
 void readInput()
 {
@@ -35,6 +37,7 @@ void readInput()
             else if (activity == LOW) {
                 state = DIALED;
                 dialed_number++;
+                delay(10);
             }
         break;
 
@@ -48,10 +51,27 @@ void readInput()
 
 void writeOutput()
 {
+    static int pos = 0;
+
     if ((generated_input) && (dialed_number > 0)) {
         dialed_number = (dialed_number == 10)? 0 : dialed_number;
+
         Serial.print("You dialed ");
         Serial.println(dialed_number);
+        if (dialed_number == password[pos]) {
+            pos++;
+            if (pos == SIZE) {
+                pos = 0;
+                Serial.println("Password correct!");
+                delay(100);
+            }
+        }
+        else {
+            pos = 0;
+            Serial.println("Try again!");
+            delay(100);
+        }
+
         generated_input = false;
         dialed_number = -1;
     }
@@ -66,7 +86,17 @@ void setup()
     pinMode(PULSE, INPUT);
     pinMode(ACTIVITY, INPUT);
     pinMode(LED, OUTPUT);
+    pinMode(A0, INPUT);
     Serial.begin(9600);
+
+    password = (int*) malloc(sizeof(int) * SIZE);
+    randomSeed(analogRead(A0));
+    for (int i = 0; i < SIZE; ++i)
+    {
+        password[i] = (int) random(0, 10);
+        Serial.print(password[i]);
+    }
+    Serial.println(";");
 }
 
 void loop()
